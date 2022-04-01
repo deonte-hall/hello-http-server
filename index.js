588eca6306b3b659ee131853fa557aadb478a38a
Deonte H (Sasko) <deonte@news-MacBook-Pro-2.local>
2022-04-01 15:18:00 -0700

    Got my server up and running! Hello World

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 
 const http = require('http');
 
+const server = http.createServer(function (req, res){
+
+    console.log('Recieved a ' + req.method + ' method on the port: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Hello World");
 
 
-const server = http.createServer(req ,res){
+})
 
-    console.log("The server is up!");
+server.listen(8080, 'localhost', null, function(){
+    console.log("Server is listening on port localhost:3000, enjoy!");
 
-    res.end("Byeeee")
-
-}
-
+})
