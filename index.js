1a650713bc6c0b2e40a772fbf5597e0101f4176b
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-04-12 12:32:15 -0700

    Just messing with port numbers to discover how my browser acts with different ports

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
 const http = require('http');
 
 const server = http.createServer(function (req, res){
-
+    var ohNo = "THE MISS";
     console.log('Recieved a ' + req.method + ' method on the port: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/plain'
+
+
+});
     res.end("Hello World");
 
 
 })
 
-server.listen(8080, 'localhost', null, function(){
-    console.log("Server is listening on port localhost:3000, enjoy!");
+server.listen(3000, 'localhost', null, function(){
+    console.log("Server is listening on port 4650, enjoy!");
 
 })
